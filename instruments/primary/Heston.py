dabf7605e825287fb8d05624abfe82ee4dffec4c
nicholasbrar <153023837+nicholasbrar@users.noreply.github.com>
2025-02-13 01:23:20 -0500

    add truncation scheme / return dict

--- instruments/primary/Heston.py
+++ instruments/primary/Heston.py
@@ -1,7 +1,10 @@
 import torch
 from torch import Tensor
+from typing import Tuple
 import numpy as np
 from instruments.primary.BasePrimary import BasePrimary
+
+
 class HestonPathsGenerator(BasePrimary):
     def __init__(self, S0: float, v0: float, rho: float, kappa: float,
                  theta: float, xi: float, mu: float, n_steps: int,
@@ -59,23 +62,38 @@ class HestonPathsGenerator(BasePrimary):
         W2 = self.rho * Z[0] + torch.sqrt(1 - self.rho ** 2) * Z[1]
         return W1 * self.sqrt_dt, W2 * self.sqrt_dt
 
-    def step(self) -> tuple[Tensor, Tensor]:
+    def step(self) -> dict:
+        def step(self) -> dict:
+            """
+            Advances the Heston model by one time step using the Full Truncation Euler Scheme to ensure non-negative variance.
+            Returns:
+                dict: A dictionary containing:
+                    - "spot" (Tensor): The simulated spot prices at the current step.
+                    - "volatility" (Tensor): The simulated variance values at the current step.
+                    - "current_step" (int): The current step in the Heston model.
+            """
+
         n_paths = self.S.shape[0]
         dW1, dW2 = self.generate_brownians(n_paths)
 
-        self.v = torch.abs(
-            self.v + self.kappa *
-            (self.theta - self.v) * self.dt +
-            self.xi * torch.sqrt(self.v) * dW2
+        v_tmp = (self.v + self.kappa
+                 * (self.theta - self.v) * self.dt
+                 + self.xi * torch.sqrt(self.v) * dW2
         )
+
+        self.v = torch.max(
+            v_tmp, torch.tensor(0.0, dtype=self.dtype, device=self.device)
+        )
+
         self.S *= torch.exp(
-            (self.mu - 0.5 * self.v) * self.dt +
-            torch.sqrt(self.v) * dW1
+            (self.mu - 0.5 * self.v) * self.dt + torch.sqrt(self.v) * dW1
         )
         self.current_step += 1
-        return self.S.clone(), self.v.clone()
 
-    def simulate(self, n_paths: int, duration: float) -> None:
+        return {"spot": self.S.clone(), "var": self.v.clone(), "nstep": self.current_step}
+
+    # Add n_steps
+    def simulate(self, n_paths: int, duration: float) -> tuple[Tensor, Tensor]:
         """
         Simulate paths for spot prices and variances for the Heston model.
         Args:
@@ -85,12 +103,12 @@ class HestonPathsGenerator(BasePrimary):
         Yields:
             (Tensor, Tensor): The current spot prices and variances at each time step
         """
-        self.reset(n_paths)
-
-        S_paths = torch.empty((self.n_steps + 1, n_paths),
-                              dtype=self.dtype, device=self.device)
-        v_paths = torch.empty((self.n_steps + 1, n_paths),
-                              dtype=self.dtype, device=self.device)
+        S_paths = torch.empty(
+            (self.n_steps + 1, n_paths), dtype=self.dtype, device=self.device
+        )
+        v_paths = torch.empty(
+            (self.n_steps + 1, n_paths), dtype=self.dtype, device=self.device
+        )
 
         S_paths[0] = self.S
         v_paths[0] = self.v
@@ -101,7 +119,6 @@ class HestonPathsGenerator(BasePrimary):
         return S_paths, v_paths
 
     def heston_generator(self, n_paths: int):
-        self.reset(n_paths)
         yield self.S.clone(), self.v.clone()
 
         while self.current_step < self.n_steps:
@@ -120,17 +137,22 @@ class HestonPathsGenerator(BasePrimary):
 
         """
         xi = self.kappa - self.rho * self.xi * 1j * u
-        d = torch.sqrt((self.rho * self.xi * 1j * u - xi) ** 2 - self.xi ** 2 * (-u * 1j - u ** 2))
-        g = (xi - self.rho * self.xi * 1j * u - d) / (xi - self.rho * self.xi * 1j * u + d)
+        d = torch.sqrt(
+            (self.rho * self.xi * 1j * u - xi) ** 2 - self.xi ** 2 * (-u * 1j - u ** 2))
+        g = (xi - self.rho * self.xi * 1j * u - d) / (
+                xi - self.rho * self.xi * 1j * u + d)
 
-        C = (
-                r * 1j * u * self.T
-                + (self.kappa * self.theta) / self.xi ** 2
-                * ((xi - self.rho * self.xi * 1j * u - d) * self.T - 2 * torch.log(
-                (1 - g * torch.exp(-d * self.T)) / (1 - g)))
+        C = r * 1j * u * self.T + (self.kappa * self.theta) / self.xi ** 2 * (
+                (xi - self.rho * self.xi * 1j * u - d) * self.T
+                - 2 * torch.log((1 - g * torch.exp(-d * self.T)) / (1 - g))
         )
+
         D = (xi - self.rho * self.xi * 1j * u - d) / self.xi ** 2 * (
                 (1 - torch.exp(-d * self.T)) / (1 - g * torch.exp(-d * self.T))
         )
 
-        return torch.exp(C + D * self.v0 + 1j * u * torch.log(self.S0))
+        return torch.exp(
+            C + D * self.v0 + 1j * u *
+            torch.log(torch.tensor(self.S0,
+                                   dtype=self.dtype, device=self.device))
+        )
