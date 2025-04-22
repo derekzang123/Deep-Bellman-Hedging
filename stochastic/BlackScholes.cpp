5582534ff1b7327b0ad3d0975abf89f9945dbf64
nicholasbrar <153023837+nicholasbrar@users.noreply.github.com>
2025-04-22 18:14:54 -0400

    Logic for variable X

--- stochastic/BlackScholes.cpp
+++ stochastic/BlackScholes.cpp
@@ -9,22 +9,26 @@ BlackScholes::BlackScholes(double S_, double K_, double r_, double q_, double T_
 rng(std::random_device{}()), dist(0.0, 1.0) {
 }
 
+double BlackScholes::price() const {
+    if (style == ExerciseStyle::American) {
+        return priceAmerican(1,2,3,4.0); // Placeholder values -- must determine n , m, l, taumax
+    }
+    return priceEuropean();
+}
+
 double BlackScholes::priceAmerican(int n, int m, int l, double tauMax) const {
     auto xVec = computeNodes(n + 1, tauMax); // Step 1
     auto tauNodes = computeCollocation(xVec);
 
-    QDPlus qd(*this, n, m, l, tauMax);
+    QDPlus qd(*this, n, m, l, tauMax); // init QD object
     qd.initBoundary();
 
     auto B = qd.getBoundary();
+
+    double X = q > r ? (K * (r/q)) : K;
+
+    
     
     // Remainder of Fast American logic
     return 0.0;
-}
-
-double BlackScholes::price() const {
-    if (style == ExerciseStyle::American) {
-        return priceAmerican(1,2,3,4.0);
-    }
-    return priceEuropean();
 }
